7bdca99f8fc126e1e7f2b3eb2e8587428e918499
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2021-05-29 09:02:14 +0530

    New Changes

--- cogs/Contest.py
+++ cogs/Contest.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands,tasks
-from .Utils import contests,constants
+from .Utils import contests,constants, table
 import asyncio
 import random
 from discord.utils import get
@@ -22,7 +22,7 @@ class Contests(commands.Cog):
 	async def on_ready(self):
 		print("Contests is online")
 
-	@tasks.loop(seconds=65)
+	@tasks.loop(seconds=100)
 	async def printer(self):
 		await self.remind()
 
@@ -32,13 +32,92 @@ class Contests(commands.Cog):
 		print('waiting...')
 		await self.client.wait_until_ready()
 
-
+	@commands.command(brief='Update Status')
 	@commands.has_role('Admin')
 	async def upd_status(self,ctx):
 		cnt = 0
 		for g in self.client.guilds:
 			cnt += len(g.members)
 		await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{cnt} members !"))
+
+	@commands.command(brief='Show future contests')
+	@commands.has_role('Admin')
+	async def future(self,ctx):
+		data = contests.getRecentContestsAll()
+		contest_list = self.client.get_channel(self.channels['All-Contests'])
+		style = table.Style('{:>}  {:<}  {:<}  {:<}  {:<}')
+		t = table.Table(style)
+		t += table.Header('#', 'Platform','Name', 'Dur', 'Countdown')
+		t += table.Line()
+		cnt=1
+
+		def convert(seconds): 
+			seconds = int(seconds)
+			seconds = seconds % (24 * 3600) 
+			hour = seconds // 3600
+			seconds %= 3600
+			minutes = seconds // 60
+			seconds %= 60
+			return "%d:%02d" % (hour, minutes) 
+
+		def convert2(seconds): 
+			seconds = int(seconds)
+			h = seconds//60
+			h = h//60
+			seconds -= h*60*60
+			m = seconds//60
+			seconds -=m*60
+			return "%d:%02d:%02d" % (h, m,seconds) 
+
+		for x in data:
+			if cnt>=10:
+				break
+			print(x[3],x[2])
+			t += table.Data(cnt, x[3], x[0], convert(x[1]) , convert2(x[2]))
+			cnt+=1
+		clist = '```\n'+str(t)+'\n```'
+		embed = discord.Embed(title='Upcoming Contests.',description=clist)
+		await contest_list.send(embed=embed)
+
+
+	async def future_update(self):
+		print("Here")
+		data = contests.getRecentContestsAll()
+		contest_list = self.client.get_channel(self.channels['All-Contests'])
+		msg = await contest_list.history(limit=1).flatten()
+		msg = msg[0]
+		style = table.Style('{:>}  {:<}  {:<}  {:<}  {:<}')
+		t = table.Table(style)
+		t += table.Header('#', 'Platform','Name', 'Dur', 'Countdown')
+		t += table.Line()
+		cnt=1
+
+		def convert(seconds): 
+			seconds = int(seconds)
+			seconds = seconds % (24 * 3600) 
+			hour = seconds // 3600
+			seconds %= 3600
+			minutes = seconds // 60
+			seconds %= 60
+			return "%d:%02d" % (hour, minutes) 
+
+		def convert2(seconds): 
+			seconds = int(seconds)
+			h = seconds//60
+			h = h//60
+			seconds -= h*60*60
+			m = seconds//60
+			seconds -=m*60
+			return "%d:%02d:%02d" % (h, m,seconds) 
+
+		for x in data:
+			if cnt>=10:
+				break
+			t += table.Data(cnt, x[3], x[0], convert(x[1]) , convert2(x[2]))
+			cnt+=1
+		clist = '```\n'+str(t)+'\n```'
+		embed = discord.Embed(title='Upcoming Contests.',description=clist)
+		await msg.edit(embed=embed)
 
 	async def clean_it(self,data):
 		new_data = []
@@ -59,12 +138,11 @@ class Contests(commands.Cog):
 			for x in data.keys():
 				channel = self.client.get_channel(self.channels[x])
 				data_foo = await self.clean_it(data[x])
-				emb = contests.getFutureContest(data_foo)
+				emb = contests.getFutureContest(data_foo,x)
 				if emb == None:
 					continue
-				role = self.client.get_guild(constants.SERVER_ID).get_role(constants.ROLE_ID)
-				await channel.send(role.mention,embed=emb)
-				all_rem_msg = await all_reminders.send(f"Reminder for {x}",embed=emb)
+				role = self.client.get_guild(constants.SERVER_ID).get_role(self.channels[x+'_Role'])
+				all_rem_msg =await channel.send(role.mention,embed=emb)
 				try:
 					# Publish_Msg
 					await all_rem_msg.publish()
@@ -72,6 +150,12 @@ class Contests(commands.Cog):
 					pass
 		except Exception as e:
 			print(e)
+		
+		try:
+			await self.future_update()
+		except Exception as e:
+			print(e)
+
 
 	async def getHistory(self):
 		channel = self.client.get_channel(self.channels["Logging"])
--- cogs/Utils/contests.py
+++ cogs/Utils/contests.py
@@ -5,7 +5,7 @@ import time
 import datetime
 
 TIME_LIMIT = 60*60+2
-
+from .constants import REMINDER_CHANNELS
 id_to_platform= {
     2:'Codechef',
     1:'Codeforces',
@@ -27,8 +27,8 @@ def get_formatted_contest_desc(id_str, start, duration, url, max_duration_len):
     return desc
 
 
-def getFutureContest(res):
-	desc = "**About to start in 1 hour <@&829804662571270205> **"
+def getFutureContest(res,platform):
+	desc = f"**About to start in 1 hour <@&{REMINDER_CHANNELS[platform+'_Role']}> **"
 	embed = discord.Embed(description=desc, color=discord.Colour.gold())
 
 	if len(res)==0:
@@ -59,8 +59,11 @@ def getFutureContest(res):
 def getNextContest():
     current_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
 	
-    url = f"https://clist.by/api/v1/contest/?start__gt={current_time}&order_by=start&username=s5960r&api_key=2e6046e37a8c58f5f13464bea345d4ef5b17acbe"
-    data = json.loads(requests.get(url).content)['objects']
+    url = f"https://clist.by/api/v1/contest/?start__gt={current_time}&order_by=start"
+    headers = {
+        'Authorization': 'ApiKey btech15060.18:534ebe6eb3d4e07ca265ca963bf2a32658efe530'
+    }
+    data = json.loads(requests.get(url, headers=headers).content)['objects']
     return data
 
 def getRecentContests():
@@ -81,4 +84,34 @@ def getRecentContests():
             else:
                 res[id_to_platform[platform]]=list()
                 res[id_to_platform[platform]].append([name,dur,start_time,link])
+    return res
+
+def scale_username(a,ln):
+	a=str(a)
+	while(len(a)<ln):
+		a=a+" "
+	longer = False
+	if len(a)>ln:
+		longer = True
+	while len(a)>ln:
+		a = a[:-1]
+	if longer:
+		a = a[:-3]
+		a += "..."
+	return a
+
+def getRecentContestsAll():
+    all_contests = getNextContest()
+    res = []
+    for data in all_contests:
+        name = data['event']
+        platform = data['resource']['id']
+        start_time = data['start']
+        link = data['href']
+        dur = data['duration']
+        time_left = int(datetime.datetime.timestamp(datetime.datetime.strptime(start_time,'%Y-%m-%dT%H:%M:%S')))
+        cur_time = datetime.datetime.timestamp(datetime.datetime.utcnow())
+        time_left= int(time_left-cur_time)
+        if platform in id_to_platform.keys() and platform!=73:
+            res.append([scale_username(name,20),dur,time_left,id_to_platform[platform]])
     return res
